c199826a6ad15ac57ccb0e4a84432e8e9e488b03
Alex <el.zingo@gmail.com>
2014-05-28 19:54:06 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alexwatt::Application.config.secret_token = '2cd74bb1097ec04ed1878e634eb745f03b16b1c92a6c0000dda028da511c319277ecd15f69ec9c23d14af5634b1aed0b790caf377f21e2687de0d6939f1938e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Alexwatt::Application.config.session_store :cookie_store, key: '_alexwatt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Alexwatt::Application.config.session_store :active_record_store
