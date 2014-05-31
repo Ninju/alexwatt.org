1ded81dbcf816a1311cb3c657b8943a3e8cb9adb
Alex <el.zingo@gmail.com>
2014-05-31 12:55:20 +0300

    Created basic games listing page for 'games' app

--- app/assets/javascripts/apps/games/list/list_view.js.coffee
+++ app/assets/javascripts/apps/games/list/list_view.js.coffee
@@ -1,6 +1,7 @@
 AlexApp.module("Games.List", (List, AlexApp, Backbone, Marionette, $, _) ->
   List.Games = Marionette.ItemView.extend({
     tagName: "ul"
+    className: "section-list"
     template: AlexApp.getTemplate("apps/games/list/list_template")
   })
 )
---
+++ app/assets/javascripts/games/lights_out.js
@@ -0,0 +1,85 @@
+$( document ).ready( function() {
+  var Cell = {
+    isOn : function() {
+      return $( this ).hasClass( "on" );
+    },
+    isOff : function() {
+      return !this.isOn();
+    },
+    flip : function() {
+      if( this.isOn() ) {
+        $( this ).removeClass( "on" ).addClass( "off" );
+      } else {
+        $( this ).removeClass( "off" ).addClass( "on" );
+      }
+    },
+    randomStatus : function() {
+      if( Math.round( Math.random() ) == 1 ) {
+        return "off";
+      }
+
+      return "on";
+    },
+    flipToRandom : function() {
+      $( this ).removeClass( "on" ).removeClass( "off" ).addClass( this.randomStatus() );
+    }
+  }
+
+  var gameOverDiv  = $( "#game_over" ),
+      resetButton  = $( "#reset" ),
+      movesText    = $( "#moves_text" ),
+      solvedText   = $( "#solved_text" ),
+      cellSelector = ".cell",
+      solved       = false;
+
+  resetButton.click( function() {
+    movesText.html( "0" );
+    gameOverDiv.hide( "slow" );
+    solved = false;
+
+    $( cellSelector ).each( function() {
+      $.extend( this, Cell );
+      this.flipToRandom();
+    } );
+  } );
+
+  $( cellSelector ).click( function() {
+    if( solved ) { return; }
+
+    var element = $( this ),
+        id      = element.attr( "id" ),
+        row     = parseInt( id.substring( 1, 2 ) ),
+        col     = parseInt( id.substring( 3, 4 ) ),
+        moves   = parseInt( movesText.html() );
+
+    movesText.html( moves + 1 );
+
+    var points = new Array();
+    points.push( [row, col] );
+
+    for( i = -1; i < 2; i++ ) {
+      points.push( [row + i, col] );
+      points.push( [row, col + i] );
+    }
+
+    for( i = 0; i < points.length; i++ ) {
+      var rowCol  = points[ i ],
+          r       = rowCol[ 0 ],
+          c       = rowCol[ 1 ],
+          element = document.getElementById( "r" + r + "c" + c );
+
+      if( element ) {
+        element.flip();
+      }
+    }
+
+    if( $( cellSelector ).filter( ".off" ).length == 0 ) {
+      solved = true;
+      gameOverDiv.show( "slow" );
+      solvedText.html( parseInt( solvedText.html() ) + 1 );
+    }
+  } );
+
+  gameOverDiv.hide();
+  resetButton.click();
+} );
