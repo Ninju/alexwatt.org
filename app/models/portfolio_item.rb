ecfbf9c251bff802371deefee4e53ab965421e0d
Alex <el.zingo@gmail.com>
2014-06-13 22:25:19 +0300

    added 'project_start_date' to portfolio items

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,5 +1,5 @@
 class PortfolioItem < ActiveRecord::Base
-  attr_accessible :description, :name, :screenshot
+  attr_accessible :description, :name, :screenshot, :project_start_date
 
   has_attached_file :screenshot
   validates_attachment_content_type :screenshot, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
