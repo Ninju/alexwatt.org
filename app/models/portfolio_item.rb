18e84d29b6b8e92b80c4dab34e2551696f95b871
Alex <el.zingo@gmail.com>
2014-06-24 23:54:43 +0300

    Mounted 'PictureUploader' onto 'screenshot' field in 'PortfolioItem' model, and commented out paperclip validations

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,6 +1,8 @@
 class PortfolioItem < ActiveRecord::Base
   attr_accessible :description, :name, :screenshot, :project_start_date
 
-  has_attached_file :screenshot
-  validates_attachment_content_type :screenshot, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  #has_attached_file :screenshot
+  #validates_attachment_content_type :screenshot, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  mount_uploader :screenshot, PictureUploader
 end
